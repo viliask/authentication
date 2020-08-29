16bb292f4edf331d3f6b12f3f906845d3c0206ed
Kamil Wilas <kamil.wilas@outlook.com>
2020-08-29 18:40:06 +0200

    Reformat code

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -12,26 +12,29 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 class ApiController extends AbstractFOSRestController
 {
     /**
-     * @Rest\Get("/api")
-    */
+     * @Rest\Get("/update-tree")
+     */
     public function updateTree(KernelInterface $kernel): JsonResponse
     {
         $list = json_decode(file_get_contents($kernel->getProjectDir().'\list.json'), true);
         $tree = json_decode(file_get_contents($kernel->getProjectDir().'\tree.json'), true);
-
-        $arrayIterator = new RecursiveArrayIterator($tree);
+        $arrayIterator     = new RecursiveArrayIterator($tree);
         $recursiveIterator = new RecursiveIteratorIterator($arrayIterator, \RecursiveIteratorIterator::SELF_FIRST);
 
         foreach ($list as $el) {
             $categoryId = intval($el['category_id']);
-            $name = $el['translations']['pl_PL']['name'];
+            $name       = $el['translations']['pl_PL']['name'];
             foreach ($recursiveIterator as $key => $value) {
                 if (is_array($value) && array_key_exists('id', $value) && $value['id'] === $categoryId) {
                     $value['name'] = $name;
-                    $currentDepth = $recursiveIterator->getDepth();
+                    $currentDepth  = $recursiveIterator->getDepth();
                     for ($subDepth = $currentDepth; $subDepth >= 0; $subDepth--) {
                         $subIterator = $recursiveIterator->getSubIterator($subDepth);
-                        $subIterator->offsetSet($subIterator->key(), ($subDepth === $currentDepth ? $value : $recursiveIterator->getSubIterator(($subDepth+1))->getArrayCopy()));
+                        $subIterator->offsetSet($subIterator->key(),
+                            ($subDepth === $currentDepth
+                                ? $value
+                                : $recursiveIterator->getSubIterator(($subDepth + 1))->getArrayCopy())
+                        );
                     }
                 }
             }
