e6db72badffbaf8bfe7e0237e54af374e02ad0e5
Kamil Wilas <kamil.wilas@outlook.com>
2020-08-31 01:59:20 +0200

    Paginate users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -16,12 +18,18 @@ class UserController extends AbstractController
     /**
      * @Route("/", name="user", methods="GET")
      */
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, Request $request, PaginatorInterface $paginator): Response
     {
         $users = $userRepository->findAll();
 
+        $result = $paginator->paginate(
+            $users,
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('user/index.html.twig', [
-            'users' => $users,
+            'users' => $result,
         ]);
     }
 
